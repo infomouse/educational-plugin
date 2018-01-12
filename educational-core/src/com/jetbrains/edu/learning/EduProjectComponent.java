36dcf4379811c759d68fcf0434ea61790d08d878
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-01-12 12:31:46 +0300

    EDU-1193 fixup! no need to return back as it's project service

--- educational-core/src/com/jetbrains/edu/learning/EduProjectComponent.java
+++ educational-core/src/com/jetbrains/edu/learning/EduProjectComponent.java
@@ -67,7 +67,6 @@ public class EduProjectComponent implements ProjectComponent {
   private FileCreatedByUserListener myListener;
   private final Map<Keymap, List<Pair<String, String>>> myDeletedShortcuts = new HashMap<>();
   private MessageBusConnection myBusConnection;
-  private String myProjectViewToSelect;
 
   private EduProjectComponent(@NotNull final Project project) {
     myProject = project;
@@ -84,7 +83,7 @@ public class EduProjectComponent implements ProjectComponent {
           return;
         }
         if (!ApplicationManager.getApplication().isUnitTestMode()) {
-          selectProjectView(ProjectViewPane.ID);
+          selectProjectView();
         }
 
         Course course = StudyTaskManager.getInstance(myProject).getCourse();
@@ -124,13 +123,12 @@ public class EduProjectComponent implements ProjectComponent {
     });
   }
 
-  private void selectProjectView(@NotNull String viewId) {
+  private void selectProjectView() {
     ProjectView projectView = ProjectView.getInstance(myProject);
     if (projectView != null) {
       String selectedViewId = ProjectView.getInstance(myProject).getCurrentViewId();
-      if (!viewId.equals(selectedViewId)) {
-        myProjectViewToSelect = selectedViewId;
-        projectView.changeView(viewId);
+      if (!ProjectViewPane.ID.equals(selectedViewId)) {
+        projectView.changeView(ProjectViewPane.ID);
       }
     }
     else {
@@ -308,9 +306,6 @@ public class EduProjectComponent implements ProjectComponent {
 
   @Override
   public void projectClosed() {
-    if (myProjectViewToSelect != null) {
-      selectProjectView(myProjectViewToSelect);
-    }
     final Course course = StudyTaskManager.getInstance(myProject).getCourse();
     if (course != null) {
       final ToolWindow toolWindow = ToolWindowManager.getInstance(myProject).getToolWindow(TaskDescriptionToolWindowFactory.STUDY_TOOL_WINDOW);
